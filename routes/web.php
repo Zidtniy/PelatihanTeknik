5699813eb791ba70aaf56cb63c6cfd92aa50f9d4
Zidtni <zidtnid@gmail.com>
2026-05-23 23:34:05 +0700

    Naruh rute web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::post('/', [HomeController::class, 'store'])->name('home.store');
+
+Route::get('/tentang', [ActivityController::class, 'index'])->name('tentang.index');
+Route::post('/tentang', [ActivityController::class, 'store'])->name('tentang.store');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
